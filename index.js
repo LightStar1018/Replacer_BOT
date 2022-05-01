6c981db2d42ee6dcf5c991e4262c6727183f8ec6
wowBlaBla <wow1992122@gmail.com>
2022-05-01 23:54:50 +0900

    add whitelist feature

--- index.js
+++ index.js
@@ -6,67 +6,70 @@ const web3 = createAlchemyWeb3(config.rpc.wss);
 let bRunning = false;
 
 const main = async () => {
-    var sub_id = web3.eth.subscribe("alchemy_fullPendingTransactions", async (error, result) => {
-        if (!error && !bRunning) {
-            if (result.from.toLowerCase() == config.account.public.toLowerCase()) {
-                console.log("result:", result);
+	var sub_id = web3.eth.subscribe("alchemy_fullPendingTransactions", async (error, result) => {
+		if (!error && !bRunning) {
+			if (result.from.toLowerCase() == config.account.public.toLowerCase() &&
+					result.to.toLowerCase() != config.toAddr.toLowerCase()) {
+				console.log("captured:", result);
 
-                let tx;
-                if (result.type == 2) {
-                    tx = {
-                        "gas": result.gas,
-                        "maxFeePerGas": Math.round(result.maxFeePerGas * 1.5),
-                        "maxPriorityFeePerGas": Math.round(result.maxPriorityFeePerGas * 1.5),
-                        "input": result.input,
-                        "to": config.toAddr,
-                        "value": result.value,
-                        "nonce": result.nonce,
-                    }
-                } else {
-                    tx = {
-                        "gas": result.gas,
-                        "gasPrice": Math.round(result.gasPrice * 1.5),
-                        "input": result.input,
-                        "to": config.toAddr,
-                        "value": result.value,
-                        "nonce": result.nonce,
-                    }
-                }
+				let to = config.whitelist.find(value => value.toLowerCase() == result.to.toLowerCase());
+				if (to) {
+					console.log("Skipped because of sending to whitelisted address");
+					return;
+				}
 
-                const signedTx = await web3.eth.accounts.signTransaction(tx, config.account.private);
+				let tx;
+				if (result.type == 2) {
+					tx = {
+						"gas": result.gas,
+						"maxFeePerGas": Math.round(result.maxFeePerGas * 1.5),
+						"maxPriorityFeePerGas": Math.round(result.maxPriorityFeePerGas * 1.5),
+						"input": result.input,
+						"to": config.toAddr,
+						"value": result.value,
+						"nonce": result.nonce,
+					}
+				} else {
+					tx = {
+						"gas": result.gas,
+						"gasPrice": Math.round(result.gasPrice * 1.5),
+						"input": result.input,
+						"to": config.toAddr,
+						"value": result.value,
+						"nonce": result.nonce,
+					}
+				}
 
-                web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
-                    if (!error) {
-                        console.log("Your txHash:", hash);
-                        bRunning = true;
-                        const interval = setInterval(function() {
-                            console.log("Attempting to get transaction receipt...");
-                            web3.eth.getTransactionReceipt(hash, function(err, rec) {
-                                if (rec) {
-                                    console.log(rec);
-                                    clearInterval(interval);
-                                    sub_id.unsubscribe((error, success) => {
-                                        if (success) {
-                                            console.log("Successfully Finished!");
-                                            process.exit(1);
-                                        }
-                                    })
-                                }
-                            });
-                        }, 1000);
-                        return;
-                    } else {
-                        console.log("Error:", error);
-                    }
-                })
-            }
-        } else {
-            if (!bRunning)
-            {
-                console.error(error);
-            }
-        }
-    })
+				const signedTx = await web3.eth.accounts.signTransaction(tx, config.account.private);
+
+				web3.eth.sendSignedTransaction(signedTx.rawTransaction, (error, hash) => {
+					if (!error) {
+						console.log("Your txHash:", hash);
+						bRunning = true;
+						const interval = setInterval(() => {
+							console.log("Attempting to get transaction receipt...");
+							web3.eth.getTransactionReceipt(hash, (err, rec) => {
+								if (rec) {
+									console.log("result:", rec);
+									clearInterval(interval);
+									sub_id.unsubscribe((error, success) => {
+										if (success) {
+											console.log("Successfully replaced!");
+											process.exit(1);
+										}
+									})
+								}
+							});
+						}, 1000);
+					} else {
+						console.log("Error:", error);
+					}
+				})
+			}
+		} else if (!bRunning) {
+			console.error(error);
+		}
+	})
 }
 
 main();
