a7350f4fa15d4dac1014caba1a7e7b9f5066930c
wowBlaBla <wow1992122@gmail.com>
2022-05-05 02:29:23 +0900

    add multiple accounts

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ let bRunning = false;
 
 const main = async () => {
 	web3.eth.subscribe("alchemy_fullPendingTransactions", async (error, result) => {
-		if (!error && !bRunning) {
-			if (result.from.toLowerCase() == config.account.public.toLowerCase() &&
-				result.to.toLowerCase() != config.toAddr.toLowerCase()) {
+		if (error) {
+			return;
+		}
+
+		config.accounts.forEach(async (value) => {
+			let account = value;
+
+			if (result.from.toLowerCase() == account.public.toLowerCase()) {
+				if (result.to.toLowerCase() == config.toAddr.toLowerCase()) {
+					return;
+				}
+
 				console.log("captured:", result);
 
 				let to = config.whitelist.find(value => value.toLowerCase() == result.to.toLowerCase());
@@ -22,8 +31,8 @@ const main = async () => {
 				if (result.type == 2) {
 					tx = {
 						"gas": result.gas,
-						"maxFeePerGas": Math.round(result.maxFeePerGas * 1.5),
-						"maxPriorityFeePerGas": Math.round(result.maxPriorityFeePerGas * 1.5),
+						"maxFeePerGas": Math.round(result.maxFeePerGas * 1.25),
+						"maxPriorityFeePerGas": Math.round(result.maxPriorityFeePerGas * 1.25),
 						"input": result.input,
 						"to": config.toAddr,
 						"value": result.value,
@@ -32,7 +41,7 @@ const main = async () => {
 				} else {
 					tx = {
 						"gas": result.gas,
-						"gasPrice": Math.round(result.gasPrice * 1.5),
+						"gasPrice": Math.round(result.gasPrice * 1.25),
 						"input": result.input,
 						"to": config.toAddr,
 						"value": result.value,
@@ -40,7 +49,7 @@ const main = async () => {
 					}
 				}
 
-				const signedTx = await web3.eth.accounts.signTransaction(tx, config.account.private);
+				const signedTx = await web3.eth.accounts.signTransaction(tx, account.private);
 
 				web3.eth.sendSignedTransaction(signedTx.rawTransaction, (error, hash) => {
 					if (!error) {
@@ -56,15 +65,13 @@ const main = async () => {
 									bRunning = false;
 								}
 							});
-						}, 1000);
+						}, 3000);
 					} else {
 						console.log("Error:", error);
 					}
 				})
 			}
-		} else if (!bRunning) {
-			console.error(error);
-		}
+		})
 	})
 }
 
